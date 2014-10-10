c069442b8806da91ef73ccce838b388f4770c64a
Bjorn Lindgren <bjorn.e.lindgren@gmail.com>
2014-10-10 17:34:17 +0200

    Add working sequetial attach test

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 CFLAGS = -Wall -g
 
-all: test_1 test_2 atomic_fifo_test
+all: test_1 test_2 atomic_fifo_test working_ll_attach
---
+++ working_ll_attach.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct atomic_fifo {
+        void *data;
+        struct atomic_fifo *next;
+};
+
+int
+main()
+{
+	struct atomic_fifo *q = NULL, *t = NULL;
+	char *d;
+
+	printf("q before alloc: %p %p\n", q, &q);
+	printf("t before alloc: %p %p\n", t, &t);
+
+	t = malloc(sizeof(struct atomic_fifo));
+
+	printf("t after alloc: %p %p\n", t, &t);
+
+	d = strdup("foobar");
+
+	t->data = (void *) d;
+	t->next = NULL;
+
+	printf("t after data add: %p %p\n", t, &t);
+	printf("t data: %s\n", (char *) t->data);
+
+	if (!q) {
+		printf("q is null\n");
+		__atomic_store(&q, &t, __ATOMIC_SEQ_CST);
+	} else {
+		printf("q is not null\n");
+		__atomic_store(&q->next, &t, __ATOMIC_SEQ_CST);
+	}
+
+	printf("q after attach: %p %p\n", q, &q);
+
+	printf("q data: %s\n", (char *) q->data);
+
+	/* second */
+
+	free(d);
+	d = strdup("barfoo");
+
+	t = malloc(sizeof(struct atomic_fifo));
+	printf("t after alloc: %p %p\n", t, &t);
+	t->data = (void *) d;
+	t->next = NULL;
+	printf("t after data add: %p %p\n", t, &t);
+	printf("t data: %s\n", (char *) t->data);
+
+	if (!q) {
+		printf("q is null\n");
+		__atomic_store(&q, &t, __ATOMIC_SEQ_CST);
+	} else {
+		printf("q is not null\n");
+		__atomic_store(&q->next, &t, __ATOMIC_SEQ_CST);
+	}
+
+	printf("q after attach: %p %p\n", q, &q);
+
+	/* third */
+
+	free(d);
+	d = strdup("fuubar");
+
+	t = malloc(sizeof(struct atomic_fifo));
+	printf("t after alloc: %p %p\n", t, &t);
+	t->data = (void *) d;
+	t->next = NULL;
+	printf("t after data add: %p %p\n", t, &t);
+	printf("t data: %s\n", (char *) t->data);
+
+	if (!q) {
+		printf("q is null\n");
+		__atomic_store(&q, &t, __ATOMIC_SEQ_CST);
+	} else {
+		printf("q is not null\n");
+		__atomic_store(&q->next, &t, __ATOMIC_SEQ_CST);
+	}
+
+	printf("q after attach: %p %p\n", q, &q);
+
+	return 0;
+}
