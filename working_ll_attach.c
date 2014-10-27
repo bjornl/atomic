570e93bbbd966504e4ad1146656a802802a72cd2
Bjorn Lindgren <bjorn.e.lindgren@gmail.com>
2014-10-27 14:34:27 +0100

    Working solution with direct access, keeping this copy with old solution using pointer to pointer.

--- working_ll_attach.c
+++ working_ll_attach.c
@@ -10,20 +10,78 @@ struct atomic_fifo {
 int
 main()
 {
-	struct atomic_fifo *q = NULL, *t = NULL;
+	struct atomic_fifo *q = NULL, *t = NULL, *h = NULL;
+	struct atomic_fifo **qp, **tp;
 	char *d;
+	int i;
 
 	printf("q before alloc: %p %p\n", q, &q);
 	printf("t before alloc: %p %p\n\n", t, &t);
 
 	t = malloc(sizeof(struct atomic_fifo));
-
 	printf("t after alloc: %p %p\n", t, &t);
-
 	d = strdup("foobar");
-
 	t->data = (void *) d;
 	t->next = NULL;
+	printf("t->next: %p %p\n", t->next, &t->next);
+
+	printf("1 tp: %p %p\n\n", tp, &tp);
+	tp = &t;
+	printf("2 tp: %p %p\n\n", tp, &tp);
+
+	//qp = &q;
+
+	//__atomic_store(qp, tp, __ATOMIC_SEQ_CST);
+	__atomic_store((struct atomic_fifo **) &q, tp, __ATOMIC_SEQ_CST);
+
+	h = q;
+
+	printf("qp: %p %p\n\n", qp, &qp);
+
+
+	printf("Data: %s\n", (char *) q->data);
+	printf("q->next: %p %p\n\n", q->next, &q->next);
+
+	/* second */
+
+	t = malloc(sizeof(struct atomic_fifo));
+	printf("t after alloc: %p %p\n", t, &t);
+	d = strdup("barfoo");
+	t->data = (void *) d;
+	t->next = NULL;
+	printf("t->next: %p %p\n", t->next, &t->next);
+
+	tp = &t;
+	//qp = &q->next;
+
+	//__atomic_store(qp, tp, __ATOMIC_SEQ_CST);
+	__atomic_store((struct atomic_fifo **) &q->next, tp, __ATOMIC_SEQ_CST);
+
+	q = q->next;
+	printf("Data: %s\n", (char *) q->data);
+
+	/* third */
+
+	t = malloc(sizeof(struct atomic_fifo));
+	printf("t after alloc: %p %p\n", t, &t);
+	d = strdup("fuubar");
+	t->data = (void *) d;
+	t->next = NULL;
+	printf("t->next: %p %p\n", t->next, &t->next);
+
+	tp = &t;
+	//qp = &q->next;
+
+	//__atomic_store(qp, tp, __ATOMIC_SEQ_CST);
+	__atomic_store((struct atomic_fifo **) &q->next, tp, __ATOMIC_SEQ_CST);
+
+	printf("Data dump:\n");
+	for (i = 1 ; i < 4 ; i++) {
+		printf("%d: %s\n", i, (char *) h->data);
+		h = h->next;
+	}
+
+exit(0);
 
 	printf("t after data add: %p %p\n", t, &t);
 	printf("t data: %s\n", (char *) t->data);
@@ -87,6 +145,7 @@ main()
 
 	printf("q after attach: %p %p\n", q, &q);
 	printf("q data: %s\n\n", (char *) q->data);
+
 
 	return 0;
 }
